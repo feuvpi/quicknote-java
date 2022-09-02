bc9599d081cdb0ea94f8513275a35588cdb0926b
feuvpi <fredvpg@gmail.com>
2022-09-02 00:24:48 -0300

    commit inicial. repositorios criados, modelos criados e conexãco com banco de dados mysql.

---
+++ app/src/main/java/model/project.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Feu's m15 R6
+ */
+public class project {
+    
+    // definindo atributos
+    
+    private int id;
+    private String name;
+    private String description;
+    private Date createdAt;
+    private Date modifiedAt;
+    
+    // gerando construtor
+
+    public project(int id, String name, String description, Date createdAt, Date modifiedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+    
+    // getters and setters 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(Date modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "project{" + "id=" + id + ", name=" + name + ", description=" + description + ", createdAt=" + createdAt + ", modifiedAt=" + modifiedAt + '}';
+    }
+    
+    
+    
+ 
+}
