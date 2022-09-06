2dbc2fd6fa9e97699bfc465b0983de9631f5a0d9
feuvpi <fredvpg@gmail.com>
2022-09-06 20:47:27 -0300

    criado metodo para puxar todos as notas de um determinado projeto.

--- app/src/main/java/controller/NoteController.java
+++ app/src/main/java/controller/NoteController.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import model.Note;
 import util.ConnectionFactory;
@@ -20,7 +22,7 @@ public class NoteController {
     
     public void save(Note note) {
         
-        String sql = "INSERT INTO notes (idProject, name, note, completed, deadline, date_creation, date_editing) "
+        String sql = "INSERT INTO tasks (idProject, name, note, completed, deadline, date_creation, date_editing) "
                 + "VALUES (?, ?, ?, ?, ?, ?, ?)";
         
         Connection c = null;
@@ -49,6 +51,38 @@ public class NoteController {
     
     public void update (Note note) {
         
+        String sql = "UPDATE notes SET "
+                + "idProject = ?, "
+                + "name = ?, "
+                + "note = ?, "
+                + "completed = ?, "
+                + "deadline = ?, "
+                + "date_editing = ? "
+                + "WHERE id = ?";
+        
+        
+        Connection c = null;
+        PreparedStatement statement = null;
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            statement = c.prepareStatement(sql);
+            statement.setInt(1, note.getIdProject());
+            statement.setString(2, note.getName());
+            statement.setString(3, note.getNote());
+            statement.setBoolean(4, note.isCompleted());
+            statement.setDate(5, new Date(note.getDeadline().getTime()));
+            statement.setDate(6, new Date(note.getModifiedAt().getTime()));
+            statement.setInt(7, note.getId());
+            statement.execute();
+                    
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao editar a nota " + ex.getMessage(), ex);
+        } finally {
+            ConnectionFactory.closeConnection(c, statement);
+       
+        }
+        
     }
     
     public void removeById(int noteId) throws SQLException{
@@ -71,6 +105,26 @@ public class NoteController {
     }
     
     public List<Note> getAll(int idProject) {
+        
+        String sql = "SELECT * from tasks WHERE idProject = ?";
+        
+        Connection c = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        List<Note> notes = new ArrayList<Note>();
+        
+        try {
+            c = ConnectionFactory.getConnection();
+            statement = c.prepareStatement(sql);
+            statement.setInt(1, idProject);
+            statement.execute();
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao carregar notas" + ex.getMessage(), ex);
+        } finally {
+            ConnectionFactory.closeConnection(c);
+        }
+        
+        
         return null;
     }
     
